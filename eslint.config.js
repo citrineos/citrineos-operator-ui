a37b98ad32cfd9620bfe205d9c625a40e12a8e26
Elliot Sabitov <>
2024-10-15 10:53:13 -0400

    chore: updating eslint config to latest version to resolve depricated linting errors that would show up in IDE

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginReact from 'eslint-plugin-react';
+import { fixupPluginRules } from '@eslint/compat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
   {
     plugins: {
-      'react-hooks': reactHooks,
+      react: eslintPluginReact,
+      'react-hooks': fixupPluginRules(reactHooks),
       'react-refresh': reactRefresh,
     },
     rules: {
