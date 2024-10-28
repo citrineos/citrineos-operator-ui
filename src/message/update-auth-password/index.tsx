29323d84d83bec7465be162c0eb2aba30be579cd
Eduardo Greb <eduardo.greb@titanclarity.com>
2024-10-28 15:40:43 -0300

    Fixing ESLint Prettier Errors

--- src/message/update-auth-password/index.tsx
+++ src/message/update-auth-password/index.tsx
@@ -9,93 +9,95 @@ import { showError, showSucces } from '../util';
 import { MessageConfirmation } from '../MessageConfirmation';
 
 export interface UpdateAuthPasswordProps {
-    station: ChargingStation;
+  station: ChargingStation;
 }
 
 enum UpdateAuthPasswordRequestProps {
-    password = "password",
-    setOnCharger = "setOnCharger",
-    stationId = "stationId"
+  password = 'password',
+  setOnCharger = 'setOnCharger',
+  stationId = 'stationId',
 }
 
 export class UpdateAuthPasswordRequest {
-    @IsNotEmpty()
-    password!: string | null;
+  @IsNotEmpty()
+  password!: string | null;
 
-    @IsNotEmpty()
-    setOnCharger!: boolean | null;
+  @IsNotEmpty()
+  setOnCharger!: boolean | null;
 }
 
 // TODO: Remove this function and use the updated triggerMessageAndHandleResponse function.
 export const triggerMessageAndHandleResponseWithDataURL = async <T,>(
-    url: string,
-    responseClass: Constructable<T>,
-    data: any,
-    responseSuccessCheck: (response: T) => boolean,
+  url: string,
+  responseClass: Constructable<T>,
+  data: any,
+  responseSuccessCheck: (response: T) => boolean,
 ) => {
-    try {
-        const client = new BaseRestClient();
-        client.setDataBaseUrl();
-        const response = await client.post(url, responseClass, {}, data);
+  try {
+    const client = new BaseRestClient();
+    client.setDataBaseUrl();
+    const response = await client.post(url, responseClass, {}, data);
 
-        if (responseSuccessCheck(response)) {
-            showSucces();
-        } else {
-            let msg = 'The request did not receive a successful response.';
-            if ((response as any).payload) {
-                msg += `Response payload: ${(response as any).payload}`;
-            }
-            showError(msg);
-        }
-    } catch (error: any) {
-        showError('The request failed with message: ' + error.message);
+    if (responseSuccessCheck(response)) {
+      showSucces();
+    } else {
+      let msg = 'The request did not receive a successful response.';
+      if ((response as any).payload) {
+        msg += `Response payload: ${(response as any).payload}`;
+      }
+      showError(msg);
     }
+  } catch (error: any) {
+    showError('The request failed with message: ' + error.message);
+  }
 };
 
 export const UpdateAuthPassword: React.FC<UpdateAuthPasswordProps> = ({
-    station,
+  station,
 }) => {
-    const [form] = Form.useForm();
-    const formProps = {
-        form,
-    };
+  const [form] = Form.useForm();
+  const formProps = {
+    form,
+  };
 
-    const updateAuthPasswordRequest = new UpdateAuthPasswordRequest();
+  const updateAuthPasswordRequest = new UpdateAuthPasswordRequest();
 
-    const handleSubmit = async (plainValues: any) => {
-        const classInstance = plainToInstance(UpdateAuthPasswordRequest, plainValues);
+  const handleSubmit = async (plainValues: any) => {
+    const classInstance = plainToInstance(
+      UpdateAuthPasswordRequest,
+      plainValues,
+    );
 
-        let data: any;
+    let data: any;
 
-        if (classInstance &&
-            classInstance[UpdateAuthPasswordRequestProps.password] &&
-            classInstance[UpdateAuthPasswordRequestProps.setOnCharger]) {
-            data = {
-                password: classInstance[
-                    UpdateAuthPasswordRequestProps.password
-                ],
-                setOnCharger: classInstance[
-                    UpdateAuthPasswordRequestProps.setOnCharger
-                ],
-                stationId: station.id
-            }
-        }
-        await triggerMessageAndHandleResponseWithDataURL(
-            `/configuration/password`,
-            MessageConfirmation,
-            data,
-            (response: MessageConfirmation) => response && response.success,
-        );
+    if (
+      classInstance &&
+      classInstance[UpdateAuthPasswordRequestProps.password] &&
+      classInstance[UpdateAuthPasswordRequestProps.setOnCharger]
+    ) {
+      data = {
+        password: classInstance[UpdateAuthPasswordRequestProps.password],
+        setOnCharger:
+          classInstance[UpdateAuthPasswordRequestProps.setOnCharger],
+        stationId: station.id,
+      };
     }
+    await triggerMessageAndHandleResponseWithDataURL(
+      `/configuration/password`,
+      MessageConfirmation,
+      data,
+      (response: MessageConfirmation) => response && response.success,
+    );
+  };
 
-    return (
-        <GenericForm
-            formProps={formProps}
-            dtoClass={UpdateAuthPasswordRequest}
-            onFinish={handleSubmit}
-            initialValues={updateAuthPasswordRequest}
-            parentRecord={updateAuthPasswordRequest}
-            gqlQueryVariablesMap={{}}
-        />
-    );;
+  return (
+    <GenericForm
+      formProps={formProps}
+      dtoClass={UpdateAuthPasswordRequest}
+      onFinish={handleSubmit}
+      initialValues={updateAuthPasswordRequest}
+      parentRecord={updateAuthPasswordRequest}
+      gqlQueryVariablesMap={{}}
+    />
+  );
 };
