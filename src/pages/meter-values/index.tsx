060e6988fcd20083b91f87e81d75597b817e5fbf
Christian Weissmann <christian.weissmann@s44.team>
2024-10-29 17:30:05 +0100

    refactor: Move to use generic table implementation for meter values

--- src/pages/meter-values/index.tsx
+++ src/pages/meter-values/index.tsx
@@ -2,19 +2,16 @@ import { ResourceType } from '../../resource-type';
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import { GenericView } from '../../components/view';
-import { useTable } from '@refinedev/antd';
-import { MeterValueListQuery } from '../../graphql/types';
 import { MeterValue } from './MeterValue';
-import { DataModelTable, IDataModelListProps } from '../../components';
+import { IDataModelListProps } from '../../components';
 import {
   METER_VALUE_CREATE_MUTATION,
   METER_VALUE_DELETE_MUTATION,
   METER_VALUE_EDIT_MUTATION,
   METER_VALUE_GET_QUERY,
-  METER_VALUE_LIST_QUERY,
 } from './queries';
-import { METER_VALUE_COLUMNS } from './table-config';
 import { BsSpeedometer } from 'react-icons/bs';
+import { GenericDataTable } from '../../components/data-model-table/editable';
 
 export const MeterValueView: React.FC = () => {
   return (
@@ -29,29 +26,7 @@ export const MeterValueView: React.FC = () => {
 };
 
 export const MeterValueList = (props: IDataModelListProps) => {
-  const { tableProps } = useTable<MeterValueListQuery>({
-    resource: ResourceType.METER_VALUES,
-    sorters: {
-      initial: [
-        {
-          field: 'timestamp',
-          order: 'desc',
-        },
-      ],
-    },
-    filters: props.filters,
-    metaData: {
-      gqlQuery: METER_VALUE_LIST_QUERY,
-    },
-  });
-
-  return (
-    <DataModelTable<MeterValue, MeterValueListQuery>
-      tableProps={tableProps}
-      columns={METER_VALUE_COLUMNS(!props.hideActions, props.parentView)}
-      hideCreateButton={props.hideCreateButton}
-    />
-  );
+  return <GenericDataTable dtoClass={MeterValue} />;
 };
 
 export const routes: React.FC = () => {
