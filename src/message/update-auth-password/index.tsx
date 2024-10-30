e40ac24336386f6085a04679ee821254be6f0f0a
Eduardo Greb <eduardo.greb@titanclarity.com>
2024-10-30 17:02:30 -0300

    refactor: refactoring Update Auth Password custom option.

--- src/message/update-auth-password/index.tsx
+++ src/message/update-auth-password/index.tsx
@@ -3,9 +3,7 @@ import { ChargingStation } from '../../pages/charging-stations/ChargingStation';
 import { IsNotEmpty } from 'class-validator';
 import { GenericForm } from '../../components/form';
 import { Form } from 'antd';
-import { Constructable } from '../../util/Constructable';
-import { BaseRestClient } from '../../util/BaseRestClient';
-import { showError, showSucces } from '../util';
+import { triggerMessageAndHandleResponse } from '../util';
 import { MessageConfirmation } from '../MessageConfirmation';
 
 export interface UpdateAuthPasswordProps {
@@ -25,32 +23,6 @@ export class UpdateAuthPasswordRequest {
   @IsNotEmpty()
   setOnCharger!: boolean | null;
 }
-
-// TODO: Remove this function and use the updated triggerMessageAndHandleResponse function.
-export const triggerMessageAndHandleResponseWithDataURL = async <T,>(
-  url: string,
-  responseClass: Constructable<T>,
-  data: any,
-  responseSuccessCheck: (response: T) => boolean,
-) => {
-  try {
-    const client = new BaseRestClient();
-    client.setDataBaseUrl();
-    const response = await client.post(url, responseClass, {}, data);
-
-    if (responseSuccessCheck(response)) {
-      showSucces();
-    } else {
-      let msg = 'The request did not receive a successful response.';
-      if ((response as any).payload) {
-        msg += `Response payload: ${(response as any).payload}`;
-      }
-      showError(msg);
-    }
-  } catch (error: any) {
-    showError('The request failed with message: ' + error.message);
-  }
-};
 
 export const UpdateAuthPassword: React.FC<UpdateAuthPasswordProps> = ({
   station,
@@ -82,12 +54,14 @@ export const UpdateAuthPassword: React.FC<UpdateAuthPasswordProps> = ({
         stationId: station.id,
       };
     }
-    await triggerMessageAndHandleResponseWithDataURL(
-      `/configuration/password`,
-      MessageConfirmation,
-      data,
-      (response: MessageConfirmation) => response && response.success,
-    );
+    await triggerMessageAndHandleResponse({
+      url: `/configuration/password`,
+      responseClass: MessageConfirmation,
+      isDataUrl: true,
+      data: data,
+      responseSuccessCheck: (response: MessageConfirmation) =>
+        response && response.success,
+    });
   };
 
   return (
