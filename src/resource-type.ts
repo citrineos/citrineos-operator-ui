7a71d69cf0b6aac2ff507823e110dcc57ba660d0
thanaParis <thana.paris@s44.team>
2025-02-03 10:49:34 -0500

    removing OCPPLog references

--- src/resource-type.ts
+++ src/resource-type.ts
@@ -6,7 +6,6 @@ export enum ResourceType {
   BOOTS = 'Boots',
   SECURITY_EVENTS = 'SecurityEvents',
   CHARGING_STATIONS = 'ChargingStations',
-  OCPP_LOGS = 'OCPPLogs',
   CHARGING_STATION_SEQUENCES = 'ChargingStationSequences',
   LOCATIONS = 'Locations',
   STATUS_NOTIFICATIONS = 'StatusNotifications',
