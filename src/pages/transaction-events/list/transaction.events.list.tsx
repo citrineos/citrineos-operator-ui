4380c502883645fc6e65604a737191051880d880
Eduardo Greb <eduardo.greb@titanclarity.com>
2025-05-14 19:54:30 -0300

    .

--- src/pages/transaction-events/list/transaction.events.list.tsx
+++ src/pages/transaction-events/list/transaction.events.list.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Table, Row } from 'antd';
 import { useTable } from '@refinedev/antd';
 import { ResourceType } from '../../../resource-type';
