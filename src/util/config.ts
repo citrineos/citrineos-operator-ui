85aba7a5fd5a96c8d155c87a7460372c2e96b07a
thanaParis <thana.paris@s44.team>
2025-06-02 18:30:16 -0400

    satisfying linter

--- src/util/config.ts
+++ src/util/config.ts
@@ -31,7 +31,8 @@ const getConfig: () => {
     googleMapsApiKey:
       window.APP_CONFIG?.VITE_GOOGLE_MAPS_API_KEY ||
       import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    hasuraAdminSecret: window.APP_CONFIG?.VITE_HASURA_ADMIN_SECRET ||
+    hasuraAdminSecret:
+      window.APP_CONFIG?.VITE_HASURA_ADMIN_SECRET ||
       import.meta.env.VITE_HASURA_ADMIN_SECRET,
     apiUrl: window.APP_CONFIG?.VITE_API_URL || import.meta.env.VITE_API_URL,
     wsUrl: window.APP_CONFIG?.VITE_WS_URL || import.meta.env.VITE_WS_URL,
