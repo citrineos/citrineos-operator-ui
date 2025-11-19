7152fd87cf4c9cbbe4f5bd851d53a9d93c580055
thana.paris@s44.team <tparis@MACUS028.local>
2025-11-18 21:55:28 -0500

    satisfying reuse

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Contributors to the CitrineOS Project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {},
   },
 };
 
