49d629bc66c479725edc663921f5d439d969a331
Devon Newsom <devon.newsom2@gmail.com>
2026-03-18 09:59:36 -0600

    linter.

--- src/lib/client/components/ui/header-banner.tsx
+++ src/lib/client/components/ui/header-banner.tsx
@@ -5,14 +5,20 @@
 import React from 'react';
 import config from '@lib/utils/config';
 
-const HeaderBanner: React.FC = () =>
+const HeaderBanner: React.FC = () => (
   <>
     {config.bannerMessage && (
       <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 dark:bg-gray-800/50 dark:after:pointer-events-none dark:after:absolute dark:after:inset-x-0 dark:after:bottom-0 dark:after:h-px dark:after:bg-white/10">
-        <div aria-hidden="true" className="absolute top-1/2 left-[max(-7rem,calc(50%-52rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl">
+        <div
+          aria-hidden="true"
+          className="absolute top-1/2 left-[max(-7rem,calc(50%-52rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+        >
           <div className="aspect-577/310 w-144.25 bg-linear-to-r from-[#ffae0b] to-[#9089fc] opacity-30 dark:opacity-40"></div>
         </div>
-        <div aria-hidden="true" className="absolute top-1/2 left-[max(45rem,calc(50%+8rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl">
+        <div
+          aria-hidden="true"
+          className="absolute top-1/2 left-[max(45rem,calc(50%+8rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+        >
           <div className="aspect-577/310 w-144.25 bg-linear-to-r from-[#ffae0b] to-[#9089fc] opacity-30 dark:opacity-40"></div>
         </div>
         <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
@@ -23,6 +29,7 @@ const HeaderBanner: React.FC = () =>
         <div className="flex flex-1 justify-end" />
       </div>
     )}
-  </>;
+  </>
+);
 
 export default HeaderBanner;
