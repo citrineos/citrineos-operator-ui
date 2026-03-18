1a21a7663f5eedb7b26a27449cb33e2a28192223
Devon Newsom <devon.newsom2@gmail.com>
2026-03-18 09:57:45 -0600

    Linter + very slight cleanup

--- src/lib/client/components/ui/header-banner.tsx
+++ src/lib/client/components/ui/header-banner.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Contributors to the CitrineOS Project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import React from 'react';
 import config from '@lib/utils/config';
 
@@ -16,9 +20,7 @@ const HeaderBanner: React.FC = () =>
             <strong className="font-semibold">{config.bannerMessage}</strong>
           </p>
         </div>
-        <div className="flex flex-1 justify-end">
-
-        </div>
+        <div className="flex flex-1 justify-end" />
       </div>
     )}
   </>;
