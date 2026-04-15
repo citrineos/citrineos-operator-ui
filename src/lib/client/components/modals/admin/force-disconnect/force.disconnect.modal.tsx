5467e79cea610a7825a29c845cdb70286a9b89a5
Thana Paris <thana.paris@s44.team>
2026-04-15 10:45:05 -0400

    fixing

--- src/lib/client/components/modals/admin/force-disconnect/force.disconnect.modal.tsx
+++ src/lib/client/components/modals/admin/force-disconnect/force.disconnect.modal.tsx
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Contributors to the CitrineOS Project
 //
 // SPDX-License-Identifier: Apache-2.0
+'use client';
 
 import { HttpMethod, type ChargingStationDto } from '@citrineos/base';
 import { ChargingStationClass } from '@lib/cls/charging.station.dto';
