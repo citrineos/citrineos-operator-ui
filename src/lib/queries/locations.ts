b914e381df1e4f7dd2df92052e978e5c439d7308
Devon Newsom <devon.newsom2@gmail.com>
2026-05-13 09:04:49 -0600

    Adds column to locations to display the ocppConnectionName for chargers Adds ocppConnectionName to locations query

--- src/lib/queries/locations.ts
+++ src/lib/queries/locations.ts
@@ -115,6 +115,7 @@ export const LOCATIONS_GET_QUERY = gql`
       openingHours
       chargingPool: ChargingStations {
         id
+        ocppConnectionName
         isOnline
         protocol
         createdAt
